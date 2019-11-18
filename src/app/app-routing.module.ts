80865c763501415fb1c4ebc7318040e5bc268942
vincenzo1094 <43038120+vincenzo1094@users.noreply.github.com>
2019-11-18 14:48:02 +0100

    Modifica sottosistema cliente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,9 @@ const routes: Routes = [
     loadChildren: () => import('./access/login/login.module').then(m => m.LoginPageModule)
   },
   { path: 'tabsCliente', loadChildren: './members/Cliente/tabs/tabs.module#TabsPageModule' },
-  //{ path: 'members', loadChildren: './members/members.module#MembersPageModule' },
   { path: 'tabsEsercente', loadChildren: './members/Esercente/tabs/tabs.module#TabsPageModule' },
-  //{ path: 'driver', loadChildren: './members/driver/driver.module#DriverPageModule' },
   { path: 'tabsDriver', loadChildren: './members/Driver/tabs/tabs.module#TabsPageModule' },
-  //{ path: 'login', loadChildren: './access/login/login.module#LoginPageModule' },
-  { path: 'register', loadChildren: './access/register/register.module#RegisterPageModule' }
+  { path: 'register', loadChildren: './access/register/register.module#RegisterPageModule' },
 ];
 @NgModule({
   imports: [
