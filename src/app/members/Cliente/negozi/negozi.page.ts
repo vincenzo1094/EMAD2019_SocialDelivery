70ba9c88b2b1a175912bce2196ff39af03f0bae5
Domenico Taffuri <domenicotaffuri94@gmail.com>
2019-12-04 11:41:20 +0100

    Aggiunti negozi e prodotti

--- src/app/members/Cliente/negozi/negozi.page.ts
+++ src/app/members/Cliente/negozi/negozi.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-negozi',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NegoziPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+
+
+  goToProdotti() {
+    this.router.navigate(['../prodotti']);
   }
 
 }
