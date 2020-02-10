38aa5de2fc8a79d2220d44876158450ccbdae17b
vincenzo1094 <43038120+vincenzo1094@users.noreply.github.com>
2020-02-10 12:19:19 +0100

    change map in cliente

--- src/app/members/Cliente/negozi/negozi.page.ts
+++ src/app/members/Cliente/negozi/negozi.page.ts
@@ -38,7 +38,7 @@ export class NegoziPage implements OnInit, AfterViewInit {
       const map = new google.maps.Map(this.mapNativeElement.nativeElement, {
         center: {lat: -34.397,  lng: 150.644},
         zoom: 16,
-        //disableDefaultUI: true
+        disableDefaultUI: true
       });
 
       /*location object*/
